a3ba72f2cda42624dc4b417a7fcb2eda1b0d3a71
hamadehd <dina.hamadeh@edu.esiee.fr>
2025-11-05 11:23:37 +0100

    commentaire ajouter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import data.clean_data as clean_data
-
+import src.analyse_test as analyse_test
 def main():
-    clean_data.main()
+    #clean_data.main()
+    analyse_test.main()
 
 if __name__ == "__main__":
     main()
