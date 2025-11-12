7f3369b44b5adf245c7189a3e67fa8001c747c6c
hamadehd <dina.hamadeh@edu.esiee.fr>
2025-11-12 19:41:51 +0100

    màj du dashboard fonctionnel avec les 2 cartes

--- src/dashboard.py
+++ src/dashboard.py
@@ -2,84 +2,87 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import config
+#from src.layout.choix_cartes import create_layout_for_carte
 from src.hierarchiepatho import get_patho_hierarchy
-from src.carte_region import lecture_BDD, creation_carte_region
+from src.page.carte_dept import creation_carte
+from src.lecture_BDD import lecture_BDD
 
 # 1. Initialisation et chargement des options statiques
 app = dash.Dash(__name__)
 PATHOS_HIERARCHY, NIV1_OPTIONS = get_patho_hierarchy()
 
-# 2. Layout (mise en page)
-# ... (Le code du Layout avec les 4 menus déroulants : Année, Niv1, Niv2, Niv3) ...
-# C'est la structure que nous avons définie à l'étape 2 précédente.
-# ... (Imports et initialisation de app) ...
-
-# MISE EN PAGE (LAYOUT)
 app.layout = html.Div([
-    html.H1("Dashboard de Prévalence Hiérarchique", style={'textAlign': 'center'}),
-    
-    html.Div([
-        # Sélecteur d'Année (inchangé)
-        html.Div([
-            html.Label("Année :"),
-            dcc.Dropdown(id='selecteur-annee', 
-                         options=[{'label':i, 'value': i}for i in config.ANNEE],
-                          value= config.ANNEE[0],
-                           clearable=False ), # Configuration inchangée
-        ], style={'width': '24%', 'display': 'inline-block'}),
-        html.Div([
-            html.Label("Sexe :"),
-            dcc.Dropdown(id='selecteur-sexe', 
-                         options=[{'label':i, 'value': i}for i in config.SEXE],
-                          value= config.SEXE[-1],
-                           clearable=False ), # Configuration inchangée
-            ], style={'width': '24%', 'display': 'inline-block'}),
-        html.Div([
-            html.Label("Âge :"),
-            dcc.Dropdown(id='selecteur-age', 
-                         options=[{'label':i, 'value': i}for i in config.AGE],
-                          value= config.AGE[-1],
-                           clearable=False ), # Configuration inchangée
-        ], style={'width': '24%', 'display': 'inline-block'}),
-        # NOUVEAU : Sélecteur Patho_Niv1
-        html.Div([
-            html.Label("Pathologie - Niveau 1 :"),
-            dcc.Dropdown(
-                id='selecteur-patho-niv1',
-                options=[{'label': i, 'value': i} for i in NIV1_OPTIONS], # Utilise les options extraites
-                value=NIV1_OPTIONS[0],
-                clearable=False
-            ),
-        ], style={'width': '24%', 'display': 'inline-block'}),
+        html.H1("Dashboard de Prévalence Hiérarchique", style={'textAlign': 'center'}),
+            html.Div([
+                html.Label("Type de Carte :"),
+                dcc.Dropdown(
+                    id='selecteur-carte',
+                    options=[
+                        {'label': 'Carte par Région', 'value': 'region'},
+                        {'label': 'Carte par Département', 'value': 'departement'}
+                    ],
+                    value='region',
+                    clearable=False
+                ),
+            ], style={'width': '24%', 'display': 'inline-block', 'padding': 10}),
         
-        # NOUVEAU : Sélecteur Patho_Niv2 (sera mis à jour par Niv1)
         html.Div([
-            html.Label("Pathologie - Niveau 2 :"),
-            dcc.Dropdown(
-                id='selecteur-patho-niv2',
-                # Les options initiales seront définies par le premier callback
-                clearable=False
-            ),
-        ], style={'width': '24%', 'display': 'inline-block'}),
+            html.Div([
+                html.Label("Année :"),
+                dcc.Dropdown(id='selecteur-annee', 
+                            options=[{'label':i, 'value': i}for i in config.ANNEE],
+                            value= config.ANNEE[0],
+                            clearable=False ),
+            ], style={'width': '24%', 'display': 'inline-block'}),
+            html.Div([
+                html.Label("Sexe :"),
+                dcc.Dropdown(id='selecteur-sexe', 
+                            options=[{'label':i, 'value': i}for i in config.SEXE],
+                            value= config.SEXE[-1],
+                            clearable=False ),
+                ], style={'width': '24%', 'display': 'inline-block'}),
+            html.Div([
+                html.Label("Âge :"),
+                dcc.Dropdown(id='selecteur-age', 
+                            options=[{'label':i, 'value': i}for i in config.AGE],
+                            value= config.AGE[-1],
+                            clearable=False ),
+            ], style={'width': '24%', 'display': 'inline-block'}),
+            
+            html.Div([
+                html.Label("Pathologie - Niveau 1 :"),
+                dcc.Dropdown(
+                    id='selecteur-patho-niv1',
+                    options=[{'label': i, 'value': i} for i in NIV1_OPTIONS],
+                    value=NIV1_OPTIONS,
+                    clearable=False
+                ),
+            ], style={'width': '24%', 'display': 'inline-block'}),
+            
+            
+            html.Div([
+                html.Label("Pathologie - Niveau 2 :"),
+                dcc.Dropdown(
+                    id='selecteur-patho-niv2',
+                    
+                    clearable=False
+                ),
+            ], style={'width': '24%', 'display': 'inline-block'}),
 
-        # NOUVEAU : Sélecteur Patho_Niv3 (sera mis à jour par Niv2)
-        html.Div([
-            html.Label("Pathologie - Niveau 3 :"),
-            dcc.Dropdown(
-                id='selecteur-patho-niv3',
-                # Les options initiales seront définies par le second callback
-                clearable=False
-            ),
-        ], style={'width': '24%', 'float': 'right', 'display': 'inline-block'}),
+            
+            html.Div([
+                html.Label("Pathologie - Niveau 3 :"),
+                dcc.Dropdown(
+                    id='selecteur-patho-niv3',
+                    clearable=False
+                ),
+            ], style={'width': '24%', 'float': 'right', 'display': 'inline-block'}),
 
-    ], style={'padding': 10}),
-    
-    html.Iframe(id='carte-dynamique', style={"width": "100%", "height": "650px", "border": "none"})
-])
+        ], style={'padding': 10}),
+        
+        html.Iframe(id='carte-dynamique', style={"width": "100%", "height": "650px", "border": "none"})
+    ])
 
-# 3. Callbacks Chaînés (Logique des menus)
-# @app.callback(Output Niv2, Input Niv1)
-# ... (Code du callback pour mettre à jour Niv2 en fonction de Niv1) ...
 @app.callback(
     [Output('selecteur-patho-niv2', 'options'),
      Output('selecteur-patho-niv2', 'value')],
@@ -122,20 +125,21 @@ def set_patho_niv3_options(selected_niv1, selected_niv2):
 # 4. Callback Principal (Mise à jour de la carte)
 @app.callback(
     Output('carte-dynamique', 'srcDoc'),
-    [Input('selecteur-annee', 'value'),
+    [Input('selecteur-carte', 'value'),
+    Input('selecteur-annee', 'value'),
     Input('selecteur-patho-niv1', 'value'),
     Input('selecteur-patho-niv2', 'value'),
      Input('selecteur-patho-niv3', 'value'),
      Input('selecteur-sexe', 'value'),
      Input('selecteur-age', 'value')]
 )
-def mettre_a_jour_carte(annee, patho_niv1, patho_niv2, patho_niv3, sexe, age): 
+def mettre_a_jour_carte(carte,annee, patho_niv1, patho_niv2, patho_niv3, sexe, age): 
     if patho_niv1 is None:
         return "" 
     
     # Lecture BDD
-    df_filtre = lecture_BDD(annee, patho_niv1,  patho_niv2, patho_niv3,sexe,age )
+    df_filtre = lecture_BDD(carte, annee, patho_niv1,  patho_niv2, patho_niv3,sexe,age )
     
     # Création du titre et de la carte
     titre = f"Total Patients ({sexe}, {patho_niv1}, {annee})"
-    return creation_carte_region(df_filtre, titre)
+    return creation_carte(carte,df_filtre, titre)
